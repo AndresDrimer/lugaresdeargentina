b7a4ee8ce2472522d9141f082545d06751159c0e
AndresDrimer <andresdrimer@hotmail.com>
2023-01-28 15:51:54 -0300

    cambio de path img sacando public

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ const data = [
         googleMapUrl: "https://goo.gl/maps/5WRWLvahJwhNhJkGA",
         title:"un “glaciar” de piedra",
         description: "Tienen una forma de esculturas con más de 3.000 metros de altura, un mar de piedra pómez que recrean olas petrificadas y formas difíciles de relatar. Para muchos es un paisaje lunar pero también es considerado un paisaje más de la puna, se encuentra en el Peñón en la provincia de Catamarca al norte de Argentina.",
-        img:"../../public/images/id1.webp"
+        img:"../../images/id1.webp"
     },
     {
         id:2,
@@ -15,7 +15,7 @@ const data = [
         googleMapUrl: "https://goo.gl/maps/acidQE8DvgfWxhGa8",
         title:"Un mar de hielo",
         description: "Es sumamente admirada por su belleza y es una de las pocas en el planeta. En este glaciar podrás caminar junto a tus seres queridos con precaución.",
-        img:"../../public/images/id2.webp"
+        img:"../../images/id2.webp"
     },
     {
         id:3,
@@ -24,7 +24,7 @@ const data = [
         googleMapUrl: "https://goo.gl/maps/6McjRpPheWUST87o8",
         title:"el desierto negro",
         description: "Payunia es un sitio que sido muy poco explorado y para realizar una caminata por el terreno necesitaras la ayuda de un guía. Se encuentra en la provincia argentina al sur de Mendoza, hace miles de años los volcanes dejaron una huella permanente con la forma de un desierto de color negro en donde aflora su entorno.",
-        img: "../../public/images/id3.jpg",
+        img: "../../images/id3.jpg",
     },
 
     {
@@ -34,7 +34,7 @@ const data = [
         googleMapUrl: "https://goo.gl/maps/cDzy2mDKdkkJtX487",
         title:"las crestas del monte",
         description: "El Monte Fitz son montañas en forma de cresta de roca entre glaciares y nubes con un brillo y color admirado. Se encuentra casi en el límite entre Chile y Argentina, en los Campos de Hielo Patagónico Sur. Los turistas extranjeros aman fotografiarse en las crestas y disfrutan de un buen chapuzón en los glaciares.",
-        img: "../../public/images/id4.webp",
+        img: "../../images/id4.webp",
     },
     {
         id:5,
@@ -43,7 +43,7 @@ const data = [
         googleMapUrl: "https://goo.gl/maps/3SKnsetdc35ss6Um7",
         title:"un mar de sal",
         description: "Posee una altitud de 3.450 y se considera el paisaje más difícil de clasificar, es otro de los depósitos de sal con gran tamaño en el planeta y se encuentra en el límite de las provincias de Salta y Jujuy en Salinas Grandes.",
-        img: "../../public/images/id5.webp",
+        img: "../../images/id5.webp",
     },
 
     {
@@ -53,7 +53,7 @@ const data = [
         googleMapUrl: "https://goo.gl/maps/XHVYz2ZwVkJo62A79",
         title:"las paredes del cañón",
         description: "Este sitio deslumbra con sus magníficas paredes rojizas con extrañas geoformas situados en un área de yacimientos arqueológicos y paleontológicos. Se encuentra en la provincia de la Rioja y fue Declarado como Patrimonio de la Humanidad por la Unesco. Es una fuente de atracción de turistas de todo el mundo.",
-        img:"../../public/images/id6.webp",
+        img:"../../images/id6.webp",
     },
     
     {
@@ -63,7 +63,7 @@ const data = [
         googleMapUrl: "https://goo.gl/maps/4D4jT4kowjLWt5Jy9",
         title:"Un valle lunar",
         description: "Este magnífico valle es considerado un escenario de paisaje surrealista repleto de piedras con algún desconocido significado. Se encuentra  en el Parque Provincial de Ischigualasto al norte la provincia de San Juan, también, hay símbolos en las paredes de piedras que parecen ser algún tipo de escritura antigua.",
-        img: "../../public/images/id7.webp",
+        img: "../../images/id7.webp",
     },
 
     {
@@ -73,7 +73,7 @@ const data = [
         googleMapUrl: "https://goo.gl/maps/TNwYJDAgrEkKwgoY6",
         title:"monumentales esculturas naturales",
         description: "A este increíble valle o quebrada también se le conoce como Quebrada de Cafeyate y se encuentran magnificas formaciones rocosas como la Garganta del Diablo y rocas de color rojo erosionadas por el agua y con curiosas formas que les han hecho obtener a cada una de ellas un nombre en particular, entre ellos están: el anfiteatro, el monje, el sapo, los castillos entre otros.",
-        img:"../../public/images/id8.webp",
+        img:"../../images/id8.webp",
     }
 ]
 
